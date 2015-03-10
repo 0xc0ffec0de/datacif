df472416ba7599e76e69c38f306309abf8530709
Pedro Vaz de Mello de Medeiros <pedrovmm@gmail.com>
2015-03-10 00:50:32 -0300

    JQuery e JQuery-UI

---
+++ public/javascripts/accordion.js
@@ -0,0 +1,39 @@
+// Cria subpágina de acordeón.
+
+accordion = function(pagePrefix, prefix, numButtons) {
+
+    console.log("LIL1");
+    dialogFunction = function(pagePrefix, prefix, i) {
+        return function(event, ui) {
+            console.log("Opening dialog " + pagePrefix + "-" + i + ".html...");
+            $("#contents-" + e(prefix + "." + i)).load(pagePrefix + "-" + i + ".html", function() {
+                populateElements($("#contents-" + e(prefix + "." + i)));
+            });
+        }
+    };
+
+    radioFunction = function(prefix, i) {
+        return function(event) {
+            console.log("#dialog-" + e(prefix + "." + i));
+            $("#dialog-" + e(prefix + "." + i)).dialog("open");
+        }
+    };
+
+    console.log("LIL3");
+    $("#radioset-" + e(prefix)).buttonset();
+    console.log("accordion() called: #radioset-" + e(prefix));
+
+    for (i = 1; i <= numButtons; ++i) {
+        console.log("accordion() called: #dialog-" + e(prefix + "." + i) + ", " + i);
+        $("#dialog-" + e(prefix + "." + i)).dialog({
+            modal: true,
+            position: { my: "center top", at: "center top" },
+            autoOpen: false,
+            width: 850,
+            height: 700,
+            open: dialogFunction(pagePrefix, prefix, i)
+        });
+
+        $("#radio-" + e(prefix + "." + i)).on("click", radioFunction(prefix, i));
+    }
+}
