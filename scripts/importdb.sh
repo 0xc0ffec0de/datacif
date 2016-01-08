d74c32947bb23d99eab16147a0abcbdf7a996ef4
Pedro Vaz de Mello de Medeiros <pedro.medeiros@gmail.com>
2016-01-08 03:10:46 -0200

    Atualizando script de criação de banco de dados.

--- scripts/importdb.sh
+++ scripts/importdb.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+OLD_DIR=$PWD
+NEW_DIR=`dirname $0`
+cd $NEW_DIR
 mongoimport --jsonArray --host 127.0.0.1 --port 27017 --db pf -c itens < ../db/cif.db
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c usuarios < ../db/usuarios.db
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c cid10Grupos < ../db/cid10grupos.db
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c cid10Itens < ../db/cid10itens.db
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c profissoes < ../db/profissoes.db
+cd $OLD_DIR
