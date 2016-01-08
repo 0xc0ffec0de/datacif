4f22285146ecda0bfd9b355d53da415a42a804f9
Pedro de Medeiros <pedro.medeiros@gmail.com>
2016-01-08 16:54:27 -0200

    Diferenças no script de importação de dados no banco de dados entre Linux e MacOS.

--- scripts/importdb.sh
+++ scripts/importdb.sh
@@ -2,7 +2,10 @@
 OLD_DIR=$PWD
 NEW_DIR=`dirname $0`
 cd $NEW_DIR
-mongoimport --jsonArray --host 127.0.0.1 --port 27017 --db pf -c itens < ../db/cif.db
+
+#mongoimport --jsonArray --host 127.0.0.1 --port 27017 --db pf -c itens < ../db/cif.db # (Linux)
+mongoimport --host 127.0.0.1 --port 27017 --db pf -c itens < ../db/cif.db
+
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c usuarios < ../db/usuarios.db
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c cid10Grupos < ../db/cid10grupos.db
 mongoimport --host 127.0.0.1 --port 27017 --db pf -c cid10Itens < ../db/cid10itens.db
