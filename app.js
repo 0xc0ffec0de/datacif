583b83fd53504fc0fa56794ea7b4ae0174560f2e
Pedro Medeiros <pedro.medeiros@gmail.com>
2016-01-25 22:43:12 -0200

    Removendo pasta de views em caminho errado.

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ var passport;
 var db = require('./config/db')(function(db) {
   passport = require('./config/passport')(app, db);
   routes   = require('./config/routes')(app, db, passport);
-  //app.initialize(passport);
 });
-
-// Exibe mensagens de erro.
-//app.get('/flash', function(req, res){
-//  // Set a flash message by passing the key, followed by the value, to req.flash().
-//  req.flash('info', 'Flash is back!');
-//  res.redirect('/');
-//});
 
 module.exports = app;
--- config/express.js
+++ config/express.js
@@ -14,11 +14,11 @@ module.exports = function(passport) {
   var Constants     = require("./constants");
 
   // Configuração de visão (MVC)
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', path.join(Constants.ROOT_PATH, 'views'));
   app.set('view engine', 'jade');
 
   // uncomment after placing your favicon in /public
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  //app.use(favicon(Constants.ROOT_PATH + '/public/favicon.ico'));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
