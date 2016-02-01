3ae0cc106034dfa11f907a21bd670bb46bfad2d6
0xc0ffec0de <pedro.medeiros@gmail.com>
2016-02-01 17:48:31 -0200

    Reorganizando banco de dados de CIF.

--- scripts/cif-children-fix.py
+++ scripts/cif-children-fix.py
@@ -8,13 +8,30 @@ db = client.pf
 itens = db.itens
 #pacientes = db.pacientes
 
+new = []
 cif = []
 
 for cif1st in itens.find().sort('cif'): # 1o nível
+    #print cif1st
 
     if 'items' in cif1st and len(cif1st['items']) > 0:
 
         for cif2nd in cif1st['items']: # 2o nível
 
-            if cif2nd['cif'][:cif2nd['cif'].len - 1] != cif1st['cif']:
-                print "%s != %s" % (cif2nd['cif'], cif1st['cif'])
+            if 'items' in cif2nd and len(cif2nd['items']) > 0:
+
+                for cif3rd in cif2nd['items']: # 2o nível
+
+                    if cif3rd['cif'][:len(cif3rd['cif']) - 1] != cif2nd['cif']:
+
+                        #itens.insert_one(cif2nd)
+                        #n = cif1st['items'].index(cif2nd)
+                        #del cif1st['items'][n]
+                        #itens.replace_one({ '_id' : cif1st['_id'] }, cif1st)
+                        print "%s != %s" % (cif3rd['cif'], cif2nd['cif'])
+
+
+
+
+
+
