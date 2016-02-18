8b97d869d8c8e2a8e55860a38c173b3bee5ed4e6
Pedro Vaz de Mello de Medeiros <pedro.medeiros@gmail.com>
2016-02-18 14:33:06 -0200

    Resultado da execução do script cif-tree.py incluido.

--- scripts/cif-tree.py
+++ scripts/cif-tree.py
@@ -9,32 +9,32 @@ itens = db.itens
 pacientes = db.pacientes
 dados = db.dados
 
-print "<ul>"
+print '<ul>'
 
 for cif1st in itens.find().sort('cif'): # 1o nível
 
     if 'cif' in cif1st:
-        print "<li>%s: %s</li>\n" % (cif1st['cif'], cif1st['description'])
+        print '<li>%s: %s</li>' % (cif1st['cif'].encode('utf-8'), cif1st['description'].encode('utf-8'))
 
     if 'items' in cif1st and len(cif1st['items']) > 0:
 
-        print "<ul>\n";
+        print '<ul>';
 
         for cif2nd in cif1st['items']: # 2o nível
 
             if 'cif' in cif2nd:
-                print "<li>%s: %s</li>\n" % (cif2nd['cif'], cif2nd['description'])
+                print '<li>%s: %s</li>' % (cif2nd['cif'].encode('utf-8'), cif2nd['description'].encode('utf-8'))
 
             if 'items' in cif2nd and len(cif2nd['items']) > 0:
 
-                print "<ul>\n";
+                print '<ul>';
 
                 for cif3rd in cif2nd['items']:
 
-                    print "<li>%s: %s</li>\n" % (cif3rd['cif'], cif3rd['description'])
+                    print '<li>%s: %s</li>' % (cif3rd['cif'].encode('utf-8'), cif3rd['description'].encode('utf-8'))
 
-                print "</ul>"
+                print '</ul>'
 
-        print "</ul>"
+        print '</ul>'
 
-print "</ul>"
+print '</ul>'
